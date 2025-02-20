14f384f74a849141ab9cad37a0eb1a6cb0f344d7
Justin Howe <jwhowe02@protonmail.com>
2025-02-20 13:24:54 -0800

    Add a new page and tweak the existing pages to work with a global layout component

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>Contact Us</title>
+
+        <!-- Styles / Scripts -->
+        <link rel="stylesheet" type="text/css" href="resources/css/app.css" />
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        <h1>Contact Us</h1>
+        <p>
+            Tel: 1-555-123-0001
+            Address: 123 Second St., Yourtown, ZZ 97000
+        </p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function() {
+    return view('about', [
+        'appName' => "Laravel Test App",
+        'appVer' => "1.0.0",
+    ]);
+});
+
+Route::get('/contact', function() {
+    return view('contact');
 });
