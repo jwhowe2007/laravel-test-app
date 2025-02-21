407ae4a1dd349ed59689b3c41912bed64cf587a9
Justin Howe <jwhowe02@protonmail.com>
2025-02-21 14:58:33 -0800

    Update home page header and adde custom home page styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,19 @@
 <x-layout>
     <x-slot:title>Home Page</x-slot>
+    <x-slot:stylerefs>
+        @vite(['resources/css/home.css'])
+    </x-slot>
     <h1>Welcome Home!</h1>
-    <x-tailwind-ui-stacked-dark-nav></x-tailwind-ui-stacked-dark-nav>
+    <x-tailwind-ui-stacked-dark-nav>
+        <x-slot:header>Welcome to Justin's Laravel test bench!</x-slot>
+        <x-slot:content>
+            <div id="content-wrapper">
+                <ol>
+                    <li>Dr. Pepper</li>
+                    <li>Pepsi</li>
+                    <li>Mt. Dew</li>
+                </ol>
+            </div>
+        </x-slot:content>
+    </x-tailwind-ui-stacked-dark-nav>
 </x-layout>
